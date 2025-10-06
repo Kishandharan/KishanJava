e733f5eaa0292491bf8e4fffd5000038ee95518a
Kishan <152014111+Kishandharan@users.noreply.github.com>
2025-10-06 05:34:50 +0400

    We did it! We made the world's biggest table generatorgit.exe add .git.exe add .

---
+++ Manually_Compiled_Projects/pkg29/mathTables1.java
@@ -0,0 +1,8 @@
+public class mathTables1{
+    public static void main(String[] args) {
+        System.out.println("3"+" "+"* "+"1"+" "+"= "+3*1); // 3 * 1 = 3
+        System.out.println("3"+" "+"* "+"2"+" "+"= "+3*2); // 3 * 2 = 6
+        System.out.println("3"+" "+"* "+"3"+" "+"= "+3*3); // 3 * 3 = 9
+
+    }
+}
---
+++ Manually_Compiled_Projects/pkg29/mathTables2.java
@@ -0,0 +1,10 @@
+public class mathTables2{
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=10; i+=2){
+            System.out.println("3"+" "+"* "+i+" "+"= "+3*i);
+        }
+
+
+    }
+}
