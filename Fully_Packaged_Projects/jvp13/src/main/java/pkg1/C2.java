8814b1db4445ab4603dc9f3ce69ae1f837dcb71c
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-02-05 07:11:25 +0530

    Andaman Prisoner, Lucky Prisoners

--- Fully_Packaged_Projects/jvp13/src/main/java/pkg1/C2.java
+++ Fully_Packaged_Projects/jvp13/src/main/java/pkg1/C2.java
@@ -6,23 +6,21 @@ import java.util.List;
 public class C2 {
     public static void main(String[] args){
         List<Character> prisons = new ArrayList<>();
+        List<Integer> lucky = new ArrayList<>();
         int count1 = 10;
         for(int i = 0; i<count1; i++){
             prisons.add('C');
         }
-        System.out.println("Opening: "+prisons);
 
         for(int i=0; i<count1; i++){
            prisons.set(i, 'O');
         }
-        System.out.println("Round 1: "+prisons);
 
         for(int i=1; i<count1; i+=2){
             prisons.set(i, 'C');
         }
-        System.out.println("Round 2: "+prisons);
 
-        for(int j=2; j<10; j++){
+        for(int j=2; j<count1; j++){
             for(int i=j; i<count1; i+=j+1){
                 if(prisons.get(i) == 'O'){
                     prisons.set(i, 'C');
@@ -30,13 +28,14 @@ public class C2 {
                     prisons.set(i, 'O');
                 }
             }
-            System.out.println("Round "+(j+1)+": "+prisons);
         }
 
         for(int i=0; i<count1; i++){
             if(prisons.get(i) == 'O'){
-                System.out.println((i+1)+" Is a lucky prisoner");
+                lucky.add(i+1);
             }
         }
+
+        System.out.println(lucky+" are the lucky ones");
     }
 }
