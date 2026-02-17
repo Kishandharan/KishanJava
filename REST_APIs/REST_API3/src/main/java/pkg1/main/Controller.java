1f060bbda1668133be6cf901f8bf8a9eb1bf9feb
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-02-17 06:55:01 +0530

    REST API multiples of 7 using /multiples/7 hardcoded

--- REST_APIs/REST_API3/src/main/java/pkg1/main/Controller.java
+++ REST_APIs/REST_API3/src/main/java/pkg1/main/Controller.java
@@ -2,6 +2,7 @@ package pkg1.main;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 
 @RestController
@@ -27,5 +28,19 @@ public class Controller {
 		return multiples7;
 	}
 	
-	
+	@GetMapping("/multiples/7")	
+	public ArrayList<Integer> getMultiples7p(){
+		ArrayList<Integer> multiples7 = new ArrayList<>();
+		int product = 0;
+		
+		for(int i = 1; i<100; i++) {
+			product=i*7;
+			if(product <= 100) {
+				multiples7.add(i*7);
+			}else{
+				break;
+			}
+		}
+		return multiples7;
+	}
 }
