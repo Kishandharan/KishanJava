e919dafdee4151af18ec153d404010172b50884e
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-02-17 06:59:19 +0530

    REST API using path variable example /multiples/{num1}

--- REST_APIs/REST_API3/src/main/java/pkg1/main/Controller.java
+++ REST_APIs/REST_API3/src/main/java/pkg1/main/Controller.java
@@ -28,19 +28,19 @@ public class Controller {
 		return multiples7;
 	}
 	
-	@GetMapping("/multiples/7")	
-	public ArrayList<Integer> getMultiples7p(){
-		ArrayList<Integer> multiples7 = new ArrayList<>();
+	@GetMapping("/multiples/{num1}")	
+	public ArrayList<Integer> getMultiples(@PathVariable int num1){
+		ArrayList<Integer> multiples = new ArrayList<>();
 		int product = 0;
 		
 		for(int i = 1; i<100; i++) {
-			product=i*7;
+			product=i*num1;
 			if(product <= 100) {
-				multiples7.add(i*7);
+				multiples.add(i*num1);
 			}else{
 				break;
 			}
 		}
-		return multiples7;
+		return multiples;
 	}
 }
