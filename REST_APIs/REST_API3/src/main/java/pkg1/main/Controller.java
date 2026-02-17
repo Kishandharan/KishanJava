a4c94749c4c7c3dca246108fe91a886e4bca2ddd
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-02-17 06:49:04 +0530

    REST API multiples of 7 upto 100 using for loop and if condition

--- REST_APIs/REST_API3/src/main/java/pkg1/main/Controller.java
+++ REST_APIs/REST_API3/src/main/java/pkg1/main/Controller.java
@@ -2,6 +2,7 @@ package pkg1.main;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
 
 @RestController
 public class Controller {
@@ -9,4 +10,22 @@ public class Controller {
 	public String greet() {
 		return "Hello World to CS sir";
 	}
+	
+	@GetMapping("/multiples7")
+	public ArrayList<Integer> getMultiples7(){
+		ArrayList<Integer> multiples7 = new ArrayList<>();
+		int product = 0;
+		
+		for(int i = 1; i<100; i++) {
+			product=i*7;
+			if(product <= 100) {
+				multiples7.add(i*7);
+			}else{
+				break;
+			}
+		}
+		return multiples7;
+	}
+	
+	
 }
